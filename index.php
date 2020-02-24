0aa651e9730083baf38d9be173786b8c0d08366f
triplr-h <hhh2738@sjtu.edu.cn>
2020-02-24 20:34:36 +0800

    add files

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php
 /**
+ * Life, Love, Freedom.
  * 
- * 
+ * @package triplr
+ * @author triplr
+ * @version 0.0.1
+ * @link http://triplr.top
  */
+
+ if (!defined('__TYPECHO_ROOT_DIR__'))
+ {
+     exit;
+ }
+ $this->need('header.php');
 ?>
+
